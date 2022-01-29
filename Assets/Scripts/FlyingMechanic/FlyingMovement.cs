ec8e8ff91331127258491a296aa89c014b0f52dc
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-01-29 18:58:35 +0100

    Bird has animation, player animation progress

--- Assets/Scripts/FlyingMechanic/FlyingMovement.cs
+++ Assets/Scripts/FlyingMechanic/FlyingMovement.cs
@@ -8,14 +8,17 @@ public class FlyingMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D myRigidbody;
     [SerializeField] private CommandContainer commandContainer;
-    
+    [SerializeField] private Animator animator;
     [SerializeField] private float flySpeed = 5f;
     public float gravity;
-    
+    private Vector2 flyVector;   
 
     // Update is called once per frame
     void Update()
     {
+        flyVector.x = commandContainer.flyCommandHorizontal;
+        animator.SetFloat("Speed", flyVector.sqrMagnitude);
+        animator.SetFloat("Horizontal", commandContainer.flyCommandHorizontal);
         HandleFlying();
     }
     private void HandleFlying()
