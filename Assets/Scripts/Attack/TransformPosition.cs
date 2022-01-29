85929ab1d8077fa4b83c71939b0f313a83a612b2
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-29 14:05:46 +0100

    Cleaner Attack script

--- Assets/Scripts/Attack/TransformPosition.cs
+++ Assets/Scripts/Attack/TransformPosition.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TransformPosition : MonoBehaviour
@@ -17,14 +15,6 @@ public class TransformPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (lookDirection.movement.x <= -0.1)
-        {
-            //move transfrom
-            transform.position = new Vector2(playerPos.position.x - 1.2f, .7f);
-        }
-        else
-        {
-            transform.position = new Vector2(playerPos.position.x + 1f, .7f);
-        }
+        transform.position = lookDirection.movement.x <= -0.1 ? new Vector2(playerPos.position.x - 1.2f, .7f) : new Vector2(playerPos.position.x + 1f, .7f);
     }
 }
