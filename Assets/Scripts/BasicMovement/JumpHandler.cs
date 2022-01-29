026839673f77b5cd3fb7e7ad734bb763c2b47874
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-29 11:22:32 +0100

    Cleaned the jump script

--- Assets/Scripts/BasicMovement/JumpHandler.cs
+++ Assets/Scripts/BasicMovement/JumpHandler.cs
@@ -2,37 +2,27 @@ using UnityEngine;
 
 public class JumpHandler : MonoBehaviour
 {
-    private Rigidbody2D rigidbody;
-    private BoxCollider2D boxCollider;
+    private Rigidbody2D _rigidbody;
 
     public float jumpForce;
-
     public float rayLenght;
-    private bool isGrounded;
+    private bool _isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.DrawRay(boxCollider.bounds.center, Vector3.down * rayLenght, Color.black);
-        if (Physics2D.Raycast(transform.position, Vector2.down, rayLenght) )
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-        
-        if (Input.GetButton("Jump") && isGrounded)
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, rayLenght);
+
+        if (Input.GetButton("Jump") && _isGrounded)
         {
             //rigidbody.AddForce(new Vector2(0, jumpForce));
-            rigidbody.velocity = Vector2.up * jumpForce;
+            _rigidbody.velocity = Vector2.up * jumpForce;
         }
     }
 }
