e8d69dbd560467d7e0b230a23e23293dacb83c35
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-01-29 20:46:38 +0100

    Camera follow stuff

--- Assets/Scripts/ETc/BackgroundController.cs
+++ Assets/Scripts/ETc/BackgroundController.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
+    [SerializeField] private GameObject bird;
     [SerializeField] private WalkOnGround walkScript;
     [Range(-1f,1f)]  public float scrollSpeed = 0.5f;
     private float offset;
     private Material mat;
+    private CommandContainer commandContainer;
     void Start()
     {
         mat = GetComponent<Renderer>().material;
+        commandContainer = bird.GetComponent<CommandContainer>();
     }
+    
 
     
     void Update()
     {
-        scrollSpeed = walkScript.movement.x;
-        offset += (Time.deltaTime * scrollSpeed) / 10;
-        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
-        ScrollBackground();
+        if (bird.activeInHierarchy)
+        {
+            scrollSpeed = commandContainer.flyCommandHorizontal;
+            offset += (Time.deltaTime * scrollSpeed) / 10;
+            mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        }
+        else
+        {
+            scrollSpeed = walkScript.movement.x;
+            offset += (Time.deltaTime * scrollSpeed) / 10;
+            mat.SetTextureOffset("_MainTex", new Vector2(offset, 0)); 
+        }
     }
 
-    void ScrollBackground()
-    {
-        
-    }
+    
+    
 }
--- Assets/Scripts/Player/Camera/CameraFollow.cs
+++ Assets/Scripts/Player/Camera/CameraFollow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
+    public GameObject birdForm;
     public Vector3 offset;
 
     void FixedUpdate()
@@ -13,10 +14,15 @@ public class CameraFollow : MonoBehaviour
         {
             Vector3 camPos = transform.position;
             Vector3 desiredPos = player.transform.position;
-
+            Vector3 altPos = birdForm.transform.position;
             Vector3 smoothedPos = Vector3.Lerp(camPos, desiredPos, 0.125f);
+            Vector3 smoothedPosAlt = Vector3.Lerp(camPos, altPos, 0.125f);
 
             transform.position = new Vector3(smoothedPos.x, transform.position.y, transform.position.z);
+            if (player.activeInHierarchy == false)
+            {
+                transform.position = new Vector3(smoothedPosAlt.x, transform.position.y, transform.position.z);
+            }
         }
     }
 }
