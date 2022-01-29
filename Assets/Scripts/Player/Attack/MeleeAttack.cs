451f2851bca19439a92284ef46ae3bdc63f58aa9
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-01-29 19:25:02 +0100

    Fixed attack cooldown

--- Assets/Scripts/Player/Attack/MeleeAttack.cs
+++ Assets/Scripts/Player/Attack/MeleeAttack.cs
@@ -1,23 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour
 {
     private float _timeBtwAttacks;
-    public float startTimeBtwAttacks;
+   // public float startTimeBtwAttacks;
     [SerializeField] private Animator animator;
     public Transform attackPos;
     public LayerMask enemyMask;
     public float attackRange;
     public int damage;
+    public float attackSpeed = 1f;
+    private float attackCD = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (_timeBtwAttacks <= 0)
+        if (Time.time >= attackCD/*_timeBtwAttacks <= 0*/)
         {
             if (Input.GetButton("Fire1"))
             {
-                _timeBtwAttacks = startTimeBtwAttacks;
+               /* _timeBtwAttacks = startTimeBtwAttacks;*/
+               attackCD = Time.time + 1f / attackSpeed;
                 animator.SetTrigger("Attack");
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyMask);
                 foreach (var enemy in enemiesToDamage)
@@ -31,6 +35,8 @@ public class MeleeAttack : MonoBehaviour
             _timeBtwAttacks -= Time.deltaTime;
         }
     }
+
+    
 
     private void OnDrawGizmosSelected()
     {
