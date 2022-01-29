b274af6bbbb143b8d8333387437e60f56c2acc15
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-01-29 21:46:11 +0100

    Ranged attack updated now making damage. Also added a Devil

--- Assets/Scripts/Enemy/CloudEnemy/LightningScript.cs
+++ Assets/Scripts/Enemy/CloudEnemy/LightningScript.cs
@@ -8,7 +8,7 @@ public class LightningScript : MonoBehaviour
     // int for damage
     public float speed = 20f;
     private Rigidbody2D rb;
-    
+    public PlayerHealth playerHealth;
     private void Awake(){
         rb = GetComponent<Rigidbody2D>();
     }
@@ -18,17 +18,22 @@ public class LightningScript : MonoBehaviour
         Vector3 Direction = (FindObjectOfType<PlayerHealth>().transform.position - transform.position).normalized;
         rb.velocity = Direction * speed;
         transform.up = -Direction;
+        playerHealth = GameObject.Find("Player (1)").GetComponent<PlayerHealth>();
     }
     
     void OnTriggerEnter2D (Collider2D hitInfo)
     {
-        PlayerHealth enemy = hitInfo.GetComponent<PlayerHealth>();
-        // Grabs the enemyHealth script component
-        if (enemy != null)
+        // PlayerHealth enemy = hitInfo.GetComponent<PlayerHealth>();
+        // // Grabs the enemyHealth script component
+        // if (enemy != null)
+        // {
+        //     enemy.TakeDamage();
+        //     Destroy(gameObject);
+        // }
+        if (hitInfo.CompareTag("Player"))
         {
-            enemy.TakeDamage();
-            Destroy(gameObject);
+            playerHealth.currentHealth -= damage;
+            playerHealth.TakeDamage();
         }
-
     }
 }
