4c361dae0af2de5fd6803ba93e62147e3ec2cbc5
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-29 15:03:00 +0100

    Enemy follows player

--- Assets/Scripts/Attack/Enemy.cs
+++ Assets/Scripts/Attack/Enemy.cs
@@ -1,22 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float health = 100f;
+    public float health;
+    public float speed;
+    public float withinRange;
+
+    private CircleCollider2D awarenessCircle;
+    //private GameObject player;
+    private BoxCollider2D playerCollider;
+    private Transform playerTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        awarenessCircle = GetComponentInChildren<CircleCollider2D>();
+        //player = GameObject.FindWithTag("Player");
+        playerCollider = GameObject.FindWithTag("Player").GetComponent<BoxCollider2D>();
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        AttackRange();
+        CheckDamage();
+    }
+
+    private void CheckDamage()
+    {
         if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void AttackRange()
+    {
+        float dist = Vector2.Distance(playerTransform.position, transform.position);
+        if (dist <= withinRange)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        throw new NotImplementedException();
     }
 }
