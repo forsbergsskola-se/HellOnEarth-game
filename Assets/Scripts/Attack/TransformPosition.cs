390abc23baf258d4e39d650ad4a635f6554ab4f9
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-29 14:09:41 +0100

    Transform script is waaaays better

--- Assets/Scripts/Attack/TransformPosition.cs
+++ Assets/Scripts/Attack/TransformPosition.cs
@@ -3,16 +3,11 @@ using UnityEngine;
 public class TransformPosition : MonoBehaviour
 {
     public WalkOnGround lookDirection;
-    public MeleeAttack meleeAttack;
     public Transform playerPos;
-    
-    // Start is called before the first frame update
     void Start()
     {
         playerPos = GameObject.Find("Player-holder").GetComponent<Transform>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = lookDirection.movement.x <= -0.1 ? new Vector2(playerPos.position.x - 1.2f, .7f) : new Vector2(playerPos.position.x + 1f, .7f);
