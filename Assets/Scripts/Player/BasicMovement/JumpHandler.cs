f117e5fc6a36241c69632856d274ab563546276d
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-30 12:24:15 +0100

    Fixed jump

--- Assets/Scripts/Player/BasicMovement/JumpHandler.cs
+++ Assets/Scripts/Player/BasicMovement/JumpHandler.cs
@@ -6,7 +6,9 @@ public class JumpHandler : MonoBehaviour
 
     public float jumpForce;
     public float rayLenght;
-    private bool _isGrounded;
+    public bool _isGrounded;
+
+    public LayerMask ground;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,17 @@ public class JumpHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.DrawRay(boxCollider.bounds.center, Vector3.down * rayLenght, Color.black);
-        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, rayLenght);
+        if (Physics2D.Raycast(transform.position, Vector2.down, rayLenght, ground))
+        {
+            _isGrounded = true;
+        }
+        else
+        {
+            _isGrounded = false;
+        }
 
+        //Debug.DrawRay(boxCollider.bounds.center, Vector3.down * rayLenght, Color.black);
+         
         if (Input.GetButton("Jump") && _isGrounded)
         {
             //rigidbody.AddForce(new Vector2(0, jumpForce));
