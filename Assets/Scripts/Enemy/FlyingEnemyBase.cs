a82cd823935a710f6c85d88d931c8e15513b0526
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-01-30 12:06:55 +0100

    Added flying enemy base script

---
+++ Assets/Scripts/Enemy/FlyingEnemyBase.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyBase : MonoBehaviour
+{
+    public float health;
+    public float damage;
+    public float speed;
+    public float withinRange;
+    
+    private Transform playerTransform;
+    private Transform bPlayerTransform;
+    public PlayerHealth playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Oden").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AttackRange();
+        CheckPlayerDamage();
+    }
+
+    private void CheckPlayerDamage()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void AttackRange()
+    {
+        float dist = Vector2.Distance(playerTransform.position, transform.position);
+        if (dist <= withinRange)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            playerHealth.currentHealth -= damage;
+            playerHealth.TakeDamage();
+        }
+    }
+}
